c5f02e4ad444171ed63971da9811a37fb87ab72c
Mert Baykal <mert44bykl@gmail.com>
2023-10-16 23:07:11 +0200

    Initial Commit

---
+++ contactsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  contactsApp
+//
+//  Created by Mert Baykal on 16/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
